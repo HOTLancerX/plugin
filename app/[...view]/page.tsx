589fbb6b5ad25f1ca3c2e302cf953e4b18b73d74
HeRa Khan <heraemt@gmail.com>
2025-04-24 17:21:20 +0600

    Update page.tsx

--- app/[...view]/page.tsx
+++ app/[...view]/page.tsx
@@ -7,21 +7,22 @@ type Props = {
   };
 };
 
-export default function ViewPage({ params }: Props) {
+// Make the function async
+export default async function ViewPage({ params }: Props) {
   if (!params?.view || !Array.isArray(params.view)) {
     return notFound();
   }
 
   const path = `/${params.view.join('/')}`;
-  
+
   const staticRoutes = getRouteComponents('view');
   const staticRoute = staticRoutes.find((r) => r.route === path);
-  
+
   if (staticRoute) {
     const Component = staticRoute.component;
     return <Component />;
   }
-  
+
   const dynamicMatch = getDynamicRouteMatch('view', path);
   if (dynamicMatch) {
     const { component: Component, params: routeParams } = dynamicMatch;
